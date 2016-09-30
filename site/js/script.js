03679365d7bedf405c499207e1317430b001c565
jundolor <jun.dolor@gmail.com>
2016-09-30 23:12:30 +0800

    code normalize

--- site/js/script.js
+++ site/js/script.js
@@ -80,62 +80,57 @@ global.$dc = dc;
 
 })(window);
 
-var recyle_process_switch = function(state){
-  if(state == 'in'){
-    console.log('went in');
-    console.log(document.querySelector("#recycle_process"));
-    document.querySelector("#recycle_process h3").style.color = "#339900";
-    document.querySelector("#recycle_process > div").style.backgroundImage = "url('https://jundolor.github.io/ryokudo/site/images/recycle-02.png')";
-    document.querySelector("#recycle_process p").style.color = "#339900";
-    document.querySelector("#recycle_process p a").style.color = "#339900";
-    document.querySelector("#recycle_process").style.borderColor = "#339900";
-  }
-  else{
-    console.log('went out');
-    document.querySelector("#recycle_process h3").style.color = "#333333";
-    document.querySelector("#recycle_process > div").style.backgroundImage = "url('https://jundolor.github.io/ryokudo/site/images/recycle-01.png')";
-    document.querySelector("#recycle_process p").style.color = "#333333";
-    document.querySelector("#recycle_process p a").style.color = "#333333";
-    document.querySelector("#recycle_process").style.borderColor = "#333333";
-  }
-};
+  var recyle_process_switch = function(state){
+    img_switch(state, 'recycle_process');
 
-var rpf_switch = function(state){
-  if(state == 'in'){
-    console.log('went in');
-    console.log(document.querySelector("#recycle_plastic_fuel"));
-    document.querySelector("#recycle_plastic_fuel h3").style.color = "#339900";
-    document.querySelector("#recycle_plastic_fuel > div").style.backgroundImage = "url('https://jundolor.github.io/ryokudo/site/images/rpf-02.png')";
-    document.querySelector("#recycle_plastic_fuel p").style.color = "#339900";
-    document.querySelector("#recycle_plastic_fuel p a").style.color = "#339900";
-    document.querySelector("#recycle_plastic_fuel").style.borderColor = "#339900";
-  }
-  else{
-    console.log('went out');
-    document.querySelector("#recycle_plastic_fuel h3").style.color = "#333333";
-    document.querySelector("#recycle_plastic_fuel > div").style.backgroundImage = "url('https://jundolor.github.io/ryokudo/site/images/rpf-01.png')";
-    document.querySelector("#recycle_plastic_fuel p").style.color = "#333333";
-    document.querySelector("#recycle_plastic_fuel p a").style.color = "#333333";
-    document.querySelector("#recycle_plastic_fuel").style.borderColor = "#333333";
-  }
-};
+  };
 
-var pfw_switch = function(state){
-  if(state == 'in'){
-    console.log('went in');
-    console.log(document.querySelector("#products_from_waste"));
-    document.querySelector("#products_from_waste h3").style.color = "#339900";
-    document.querySelector("#products_from_waste > div").style.backgroundImage = "url('https://jundolor.github.io/ryokudo/site/images/prod-02.png')";
-    document.querySelector("#products_from_waste p").style.color = "#339900";
-    document.querySelector("#products_from_waste p a").style.color = "#339900";
-    document.querySelector("#products_from_waste").style.borderColor = "#339900";
+  var rpf_switch = function(state){
+    img_switch(state, 'recycle_plastic_fuel');
+
+  };
+
+  var pfw_switch = function(state){
+    img_switch(state, 'products_from_waste');
   }
-  else{
-    console.log('went out');
-    document.querySelector("#products_from_waste h3").style.color = "#333333";
-    document.querySelector("#products_from_waste > div").style.backgroundImage = "url('https://jundolor.github.io/ryokudo/site/images/prod-01.png')";
-    document.querySelector("#products_from_waste p").style.color = "#333333";
-    document.querySelector("#products_from_waste p a").style.color = "#333333";
-    document.querySelector("#products_from_waste").style.borderColor = "#333333";
-  }
-};
+
+  var img_switch = function(state, source){
+    var selector = '';
+    var img_in = '';
+    var img_out = '';
+
+    if(source == 'recycle_process'){
+      selector = '#recycle_process';
+      img_in = 'recycle-02.png';
+      img_out = 'recycle-01.png';
+
+    }
+    else if(source == 'recycle_plastic_fuel'){
+      selector = '#recycle_plastic_fuel';
+      img_in = 'rpf-02.png';
+      img_out = 'rpf-01.png';   
+
+    }
+    else{
+      selector = '#products_from_waste';
+      img_in = 'prod-02.png';
+      img_out = 'prod-01.png';   
+
+    }
+
+
+    if(state == 'in'){
+      document.querySelector(selector + " h3").style.color = "#339900";
+      document.querySelector(selector + " > div").style.backgroundImage = "url('https://jundolor.github.io/ryokudo/site/images/" + img_in + "')";
+      document.querySelector(selector + " p").style.color = "#339900";
+      document.querySelector(selector + " p a").style.color = "#339900";
+      document.querySelector(selector).style.borderColor = "#339900";
+    }
+    else{
+      document.querySelector(selector + " h3").style.color = "#333333";
+      document.querySelector(selector + " > div").style.backgroundImage = "url('https://jundolor.github.io/ryokudo/site/images/" + img_out + "')";
+      document.querySelector(selector + " p").style.color = "#333333";
+      document.querySelector(selector + " p a").style.color = "#333333";
+      document.querySelector(selector).style.borderColor = "#333333";
+    }
+  };
